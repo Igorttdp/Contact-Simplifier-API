d090bffac86869394e5dc684bffd9bab11d4c5ba
Igorttdp <igorttdp.dev@email.com>
2023-03-27 03:39:35 -0300

    feat: centralize imports

---
+++ src/middlewares/index.ts
@@ -0,0 +1,5 @@
+import validateSchemasMiddleware from "./validateSchemas.middleware";
+import ensureEmailIsValidMiddleware from "./ensureEmailIsValid.middleware";
+import ensureTokenIsValidMiddleware from "./ensureTokenIsValid.middleware";
+
+export { validateSchemasMiddleware, ensureEmailIsValidMiddleware, ensureTokenIsValidMiddleware };
