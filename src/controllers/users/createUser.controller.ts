7218f934af2765360e62e8655e14e78cbdfa4606
Igorttdp <igorttdp.dev@email.com>
2023-03-27 03:28:56 -0300

    feat: add create user controller

---
+++ src/controllers/users/createUser.controller.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import { createUserService } from "../../services/users";
+
+const createUserController = async (req: Request, res: Response) => {
+  const response = await createUserService(req.body);
+
+  return res.status(201).json(response);
+};
+
+export default createUserController;
