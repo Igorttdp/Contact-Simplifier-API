5141955476241e1d35557413fc16d6aa6777724f
Igorttdp <igorttdp.dev@email.com>
2023-03-27 03:28:19 -0300

    feat: add index to centralize imports

---
+++ src/controllers/contacts/index.ts
@@ -0,0 +1,11 @@
+import createContactController from "./createContact.controller";
+import retrieveContactsController from "./retrieveContacts.controller";
+import updateContactController from "./updateContact.controller";
+import deleteContactController from "./deleteContact.controller";
+
+export {
+  createContactController,
+  retrieveContactsController,
+  updateContactController,
+  deleteContactController,
+};
---
+++ src/controllers/users/index.ts
@@ -0,0 +1,13 @@
+import createUserController from "./createUser.controller";
+import loginUserController from "./loginUser.controller";
+import updateUserController from "./updateUser.controller";
+import userProfileController from "./userProfile.controller";
+import deleteUserController from "./deleteUser.controller";
+
+export {
+  createUserController,
+  userProfileController,
+  updateUserController,
+  deleteUserController,
+  loginUserController,
+};
