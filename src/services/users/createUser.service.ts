dfa254cde6597cb6b271ee9749a9e50fe33b6859
Igorttdp <igorttdp.dev@email.com>
2023-03-27 03:46:10 -0300

    feat: add create user service to create an user

---
+++ src/services/users/createUser.service.ts
@@ -0,0 +1,21 @@
+import { IUserRequest } from "../../interfaces/user.interface";
+import { User } from "../../entities/user.entity";
+import AppDataSource from "../../data-source";
+import { createUserResponseSchema } from "../../schemas/createUser.schemas";
+
+const createUserService = async (payload: IUserRequest) => {
+  const userRepository = AppDataSource.getRepository(User);
+  const user = userRepository.create(payload);
+  await userRepository.save(user);
+
+  console.log(user);
+
+  const validatedBody = createUserResponseSchema.validateSync(user, {
+    abortEarly: false,
+    stripUnknown: true,
+  });
+
+  return validatedBody;
+};
+
+export default createUserService;
