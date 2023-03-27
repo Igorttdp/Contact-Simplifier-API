3f1773e1a75341747ba0be3f99a00d9627f6d71d
Igorttdp <igorttdp.dev@email.com>
2023-03-27 03:48:43 -0300

    feat: centralize imports

---
+++ src/services/users/index.ts
@@ -0,0 +1,13 @@
+import createUserService from "./createUser.service";
+import loginService from "./login.service";
+import userProfileService from "./userProfile.service";
+import updateUserService from "./updateUser.service";
+import deleteUserService from "./deleteUser.service";
+
+export {
+  createUserService,
+  userProfileService,
+  updateUserService,
+  deleteUserService,
+  loginService,
+};
