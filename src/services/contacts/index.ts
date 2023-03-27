e65d346aae406f857d69b1658993a5af989298fc
Igorttdp <igorttdp.dev@email.com>
2023-03-27 03:45:36 -0300

    feat: centralize imports

---
+++ src/services/contacts/index.ts
@@ -0,0 +1,6 @@
+import createContactService from "./createContact.service";
+import retrieveContactsService from "./retrieveContacts.service";
+import updateContactService from "./updateContact.service";
+import deleteContactService from "./deleteContact.service";
+
+export { createContactService, retrieveContactsService, updateContactService, deleteContactService };
