8b649ff2a11bd183ec06837b333bbf202acd7dd6
Igorttdp <igorttdp.dev@email.com>
2023-04-05 13:36:02 -0300

    feat: add cors origin settings

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,12 @@ import cors from "cors";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://contact-simplifier.vercel.app/", "localhost:3000"],
+    credentials: true,
+  })
+);
 
 app.use("", userRoutes);
 app.use("", contactRoutes);
